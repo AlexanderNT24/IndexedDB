d4508394f33c2463d58d2566f608809896762616
Alexander <89727568+AlexanderNT24@users.noreply.github.com>
2022-02-18 02:45:50 -0500

    add update, delete and fix views

--- index.js
+++ index.js
@@ -1,107 +1,171 @@
-let db
+let db;
+const list = document.querySelector("td");
+document.getElementById("buttonView").style.display = "none";
 
 document.getElementById("buttonInsert").onclick = () => {
   const name = document.getElementById("name").value;
   const dni = document.getElementById("dni").value;
   const birthDate = document.getElementById("birthDate").value;
-  data = { dni: dni, name: name, birthDate: birthDate }
-  agregar(data)
+  data = { dni: dni, name: name, birthDate: birthDate };
+  addData(data);
 };
 
-document.getElementById("buttonShow").onclick = () => {
-  consult()
-};
-
-document.getElementById("buttonDeleteUser").onclick = () => {
-  const dni = document.getElementById("deleteDniUser").value;
-  deleteData(dni)
-};
 document.getElementById("buttonSearchUser").onclick = () => {
   const dni = document.getElementById("searchDniUser").value;
-  get(dni)
+  document.getElementById("buttonView").style.display = "block";
+  getDataUser(dni);
 };
 
-function addElement(text) {
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode(text);
-  newDiv.appendChild(newContent);
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
+document.getElementById("buttonView").onclick = () => {
+  consult();
+  document.getElementById("buttonView").style.display = "none";
+};
+
+function addElement(nameData, dniData, brithDateData) {
+  const listItem = document.createElement("li");
+  const name = document.createElement("h3");
+  const dni = document.createElement("h3");
+  const birthDate = document.createElement("h3");
+
+  listItem.appendChild(name);
+  listItem.appendChild(dni);
+  listItem.appendChild(birthDate);
+  list.appendChild(listItem);
+
+  // Coloca los datos del cursor dentro de h3 y para
+  name.textContent = "Nombre: " + nameData;
+  name.style = "font-style: italic;";
+  dni.textContent = "DNI: " + dniData;
+  dni.style = "font-style: italic;";
+  birthDate.textContent = "Fecha Nacimiento: " + brithDateData;
+  birthDate.style = "font-style: italic;";
+
+  // Crea un botón y lo coloca dentro de cada listItem
+  const deleteBtn = document.createElement("button");
+  deleteBtn.onclick = () => {
+    console.log("elimino");
+    const trasaccion = db.transaction(["users"], "readwrite");
+    const coleccionObjetos = trasaccion.objectStore("users");
+    console.log(dniData);
+    const conexion = coleccionObjetos.delete(dniData);
+
+    conexion.onsuccess = () => {
+      consult();
+    };
+  };
+  deleteBtn.textContent = "Eliminar";
+  deleteBtn.className = "button small";
+  listItem.appendChild(deleteBtn);
+
+  const updateButton = document.createElement("button");
+  updateButton.textContent = "Actualizar";
+  updateButton.className = "button small";
+  updateButton.onclick = () => {
+    const updateName = document.createElement("input");
+    updateName.type = "text";
+    updateName.placeholder = "Ingresa tu nombre";
+    updateName.id = "updateName";
+    listItem.appendChild(updateName);
+
+    const updateBirthDate = document.createElement("input");
+    updateBirthDate.type = "date";
+    updateBirthDate.id = "updateBirthDate";
+    listItem.appendChild(updateBirthDate);
+
+    const updateButtonSend = document.createElement("button");
+    updateButtonSend.className = "button small";
+    updateButtonSend.type = "submit";
+    updateButtonSend.id = "updateButtonSend";
+    updateButtonSend.textContent = "Guardar";
+    listItem.appendChild(updateButtonSend);
+
+    document.getElementById("updateButtonSend").onclick = () => {
+      const name = document.getElementById("updateName").value;
+      const dni = dniData;
+      const birthDate = document.getElementById("updateBirthDate").value;
+      data = { dni: dni, name: name, birthDate: birthDate };
+
+      const trasaccion = db.transaction(["users"], "readwrite");
+      const coleccionObjetos = trasaccion.objectStore("users");
+      const conexion = coleccionObjetos.put(data);
+      consult();
+    };
+  };
+  listItem.appendChild(updateButton);
 }
 
-const connection = window.indexedDB.open('users', 1);
+const connection = window.indexedDB.open("users", 1);
 
 connection.onsuccess = () => {
-  db = connection.result
-  console.log('Base de datos abierta', db)
-}
+  db = connection.result;
+  consult();
+  console.log("Base de datos abierta", db);
+};
 
 connection.onerror = (error) => {
-  console.log('Error ', error)
-}
+  console.log("Error ", error);
+};
 
 connection.onupgradeneeded = (e) => {
-  db = e.target.result
-  console.log('Base de datos creada', db)
-  const coleccionObjetos = db.createObjectStore('users', {
-    keyPath: 'dni'
-  })
-}
+  db = e.target.result;
+  console.log("Base de datos creada", db);
+  const coleccionObjetos = db.createObjectStore("users", {
+    keyPath: "dni",
+  });
+};
 
+const addData = (info) => {
+  const trasaccion = db.transaction(["users"], "readwrite");
+  const coleccionObjetos = trasaccion.objectStore("users");
+  const conexion = coleccionObjetos.add(data);
+  consult();
+};
 
-const agregar = (info) => {
-  const trasaccion = db.transaction(['users'], 'readwrite')
-  const coleccionObjetos = trasaccion.objectStore('users')
-  const conexion = coleccionObjetos.add(data)
-  consult()
-}
-
-const get = (dni) => {
-  const trasaccion = db.transaction(['users'], 'readonly')
-  const coleccionObjetos = trasaccion.objectStore('users')
-  const conexion = coleccionObjetos.get(dni)
+const getDataUser = (dni) => {
+  deleteAllElements();
+  const trasaccion = db.transaction(["users"], "readonly");
+  const coleccionObjetos = trasaccion.objectStore("users");
+  const conexion = coleccionObjetos.get(dni);
 
   conexion.onsuccess = (e) => {
-    addElement("Name: " + conexion.result.name)
-    addElement("DNI: " + conexion.result.dni)
-    addElement("Birth Date: " + conexion.result.birthDate)
+    addElement(
+      conexion.result.name,
+      conexion.result.dni,
+      conexion.result.birthDate
+    );
 
-    console.log(conexion.result)
-  }
+    console.log(conexion.result);
+  };
   conexion.onerror = (e) => {
-    addElement("No found user with dni")
-
-  }
-
-}
-
-const deleteData = (dni) => {
-  const trasaccion = db.transaction(['users'], 'readwrite')
-  const coleccionObjetos = trasaccion.objectStore('users')
-  const conexion = coleccionObjetos.delete(dni)
-
-  conexion.onsuccess = () => {
-    consult()
-  }
-}
+    addElement("No found user with dni");
+  };
+};
 
 const consult = () => {
-  const trasaccion = db.transaction(['users'], 'readonly')
-  const coleccionObjetos = trasaccion.objectStore('users')
-  const conexion = coleccionObjetos.openCursor()
+  deleteAllElements();
+  const trasaccion = db.transaction(["users"], "readonly");
+  const coleccionObjetos = trasaccion.objectStore("users");
+  const conexion = coleccionObjetos.openCursor();
 
-
+  var boolCursor = false;
   conexion.onsuccess = (e) => {
-    const cursor = e.target.result
+    const cursor = e.target.result;
     if (cursor) {
-      console.log(cursor.value)
-      addElement("Name: " + cursor.value.name)
-      addElement("DNI: " + cursor.value.dni)
-      addElement("Birth Date: " + cursor.value.birthDate)
-      addElement("---------------------")
-      cursor.continue()
-    } else {
-      addElement("No hay usuarios")
+      console.log(cursor.value);
+      boolCursor = true;
+      addElement(cursor.value.name, cursor.value.dni, cursor.value.birthDate);
+      cursor.continue();
     }
+    if (boolCursor) {
+      document.getElementById("noElementsSpan").style.display = "none";
+    } else {
+      document.getElementById("noElementsSpan").style.display = "block";
+    }
+  };
+};
+
+const deleteAllElements = () => {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
   }
-}
+};
