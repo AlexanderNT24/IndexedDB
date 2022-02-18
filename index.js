5e7552bcb327919ed2d56671a1ec8a2d4607d494
Alexander <89727568+AlexanderNT24@users.noreply.github.com>
2022-02-18 00:43:45 -0500

    fix views with css foundation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let db
 
 document.getElementById("buttonInsert").onclick = () => {
-  const name=document.getElementById("name").value;
-  const dni=document.getElementById("dni").value;
-  const birthDate=document.getElementById("birthDate").value;
-  data={dni:dni, name:name, birthDate:birthDate}
+  const name = document.getElementById("name").value;
+  const dni = document.getElementById("dni").value;
+  const birthDate = document.getElementById("birthDate").value;
+  data = { dni: dni, name: name, birthDate: birthDate }
   agregar(data)
 };
 
@@ -13,95 +13,95 @@ document.getElementById("buttonShow").onclick = () => {
 };
 
 document.getElementById("buttonDeleteUser").onclick = () => {
-  const dni=document.getElementById("deleteDniUser").value;
+  const dni = document.getElementById("deleteDniUser").value;
   deleteData(dni)
 };
 document.getElementById("buttonSearchUser").onclick = () => {
-  const dni=document.getElementById("searchDniUser").value;
+  const dni = document.getElementById("searchDniUser").value;
   get(dni)
 };
 
-function addElement (text) {
+function addElement(text) {
   var newDiv = document.createElement("div");
   var newContent = document.createTextNode(text);
-  newDiv.appendChild(newContent); 
+  newDiv.appendChild(newContent);
   var currentDiv = document.getElementById("div1");
   document.body.insertBefore(newDiv, currentDiv);
 }
 
-const connection = window.indexedDB.open('users',1);
+const connection = window.indexedDB.open('users', 1);
 
-connection.onsuccess = () =>{
+connection.onsuccess = () => {
   db = connection.result
   console.log('Base de datos abierta', db)
 }
 
-connection.onerror = (error) =>{
+connection.onerror = (error) => {
   console.log('Error ', error)
 }
 
-connection.onupgradeneeded = (e) =>{
+connection.onupgradeneeded = (e) => {
   db = e.target.result
   console.log('Base de datos creada', db)
-  const coleccionObjetos = db.createObjectStore('users',{
-      keyPath: 'dni'
-    })
+  const coleccionObjetos = db.createObjectStore('users', {
+    keyPath: 'dni'
+  })
 }
 
 
 const agregar = (info) => {
-  const trasaccion = db.transaction(['users'],'readwrite')
+  const trasaccion = db.transaction(['users'], 'readwrite')
   const coleccionObjetos = trasaccion.objectStore('users')
   const conexion = coleccionObjetos.add(data)
   consult()
 }
 
-const get = (dni) =>{
-  const trasaccion = db.transaction(['users'],'readonly')
+const get = (dni) => {
+  const trasaccion = db.transaction(['users'], 'readonly')
   const coleccionObjetos = trasaccion.objectStore('users')
   const conexion = coleccionObjetos.get(dni)
 
-  conexion.onsuccess = (e) =>{
-    addElement("Name: "+conexion.result.name)
-    addElement("DNI: "+conexion.result.dni)
-    addElement("Birth Date: "+conexion.result.birthDate)
-    
-      console.log(conexion.result)
+  conexion.onsuccess = (e) => {
+    addElement("Name: " + conexion.result.name)
+    addElement("DNI: " + conexion.result.dni)
+    addElement("Birth Date: " + conexion.result.birthDate)
+
+    console.log(conexion.result)
   }
-  conexion.onerror = (e) =>{
+  conexion.onerror = (e) => {
     addElement("No found user with dni")
-    
+
   }
-  
+
 }
 
-const deleteData = (dni) =>{      
-  const trasaccion = db.transaction(['users'],'readwrite')
+const deleteData = (dni) => {
+  const trasaccion = db.transaction(['users'], 'readwrite')
   const coleccionObjetos = trasaccion.objectStore('users')
   const conexion = coleccionObjetos.delete(dni)
 
-  conexion.onsuccess = () =>{
-      consult()
+  conexion.onsuccess = () => {
+    consult()
   }
 }
 
-const consult = () =>{
-  const trasaccion = db.transaction(['users'],'readonly')
+const consult = () => {
+  const trasaccion = db.transaction(['users'], 'readonly')
   const coleccionObjetos = trasaccion.objectStore('users')
   const conexion = coleccionObjetos.openCursor()
 
 
-  conexion.onsuccess = (e) =>{
-      const cursor = e.target.result
-      if(cursor){
-          console.log(cursor.value)
-          addElement("Name: "+cursor.value.name)
-          addElement("DNI: "+cursor.value.dni)
-          addElement("Birth Date: "+cursor.value.birthDate)
-          addElement("---------------------")
-          cursor.continue()
-      }else{
-        addElement("No hay usuarios")
-      }
+  conexion.onsuccess = (e) => {
+    const cursor = e.target.result
+    if (cursor) {
+      console.log(cursor.value)
+      addElement("Name: " + cursor.value.name)
+      addElement("DNI: " + cursor.value.dni)
+      addElement("Birth Date: " + cursor.value.birthDate)
+      addElement("---------------------")
+      cursor.continue()
+    } else {
+      addElement("No hay usuarios")
+    }
   }
 }
